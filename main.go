02b62829fa989ec9740b85c2ef9a08538bdc40d5
piyush1146115 <piyush123kantidas@gmail.com>
2023-03-28 22:01:22 +0600

    Add docker and docker-compose.

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/piyush1146115/dummy-bank-golang/db/util"
+	"log"
+)
+
+func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load confif:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
+	if err != nil {
+		log.Fatal("can not connect to DB:", err)
+	}
+
+	if err := conn.Ping(); err != nil {
+		log.Fatal("failed to ping db")
+	}
+}
